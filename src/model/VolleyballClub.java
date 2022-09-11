872527d17a05bc7a0848bf8a5af01682e5570cd8
somaye6868 <somayekariminia6868@gmail.com>
2022-09-11 23:31:17 +0430

    update VolleyballClub

--- src/model/VolleyballClub.java
+++ src/model/VolleyballClub.java
@@ -1,14 +1,22 @@
 package model;
 
-public class VolleyballClub extends Club  {
+import java.util.ArrayList;
+import java.util.List;
+
+public class VolleyballClub extends Club {
     private int setWinCount;
     private int setLossCount;
+    private List<Play> plays = new ArrayList<>();
 
     public VolleyballClub(String name, int plays, double score, int winCount, int lossCount) {
         super(name, plays, score, winCount, lossCount);
     }
 
     public int getSetWinCount() {
+        for (Play play:plays)
+        {
+         setWinCount+=play.getGoalCountFirst();
+        }
         return setWinCount;
     }
 
@@ -17,10 +25,36 @@ public class VolleyballClub extends Club  {
     }
 
     public int getSetLossCount() {
+        for (Play play:plays)
+        {
+         setLossCount+=play.getGoalCountSecond();
+        }
         return setLossCount;
     }
 
     public void setSetLossCount(int setLossCount) {
         this.setLossCount = setLossCount;
     }
+
+    @Override
+    public int getWinCount() {
+        int winCount=0;
+        for (Play play:plays) {
+            if(play.getResult().equals(Result.WIN))
+                winCount++;
+        }
+        return winCount;
+
 }
+
+    @Override
+    public int getLossCount() {
+        int lossCount=0;
+        for (Play play:plays) {
+            if(play.getResult().equals(Result.LOST))
+                lossCount++;
+        }
+        return lossCount;
+    }
+
+    }
