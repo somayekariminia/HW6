ebe70f5e8c1602d915675a69fb83713dcfe90175
somaye6868 <somayekariminia6868@gmail.com>
2022-09-11 18:15:02 +0430

    add Play class

---
+++ src/model/Play.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Play {
+    private String nameTeamFirst;
+    private String nameTeamSecond;
+    private int goalCountFirst;
+    private  int goalCountSecond;
+    private Result result;
+
+    public Play(String nameTeamFirst, String nameTeamSecond, int goalCountFirst, int goalCountSecond, Result result) {
+        this.nameTeamFirst = nameTeamFirst;
+        this.nameTeamSecond = nameTeamSecond;
+        this.goalCountFirst = goalCountFirst;
+        this.goalCountSecond = goalCountSecond;
+        this.result = result;
+    }
+
+    public String getNameTeamFirst() {
+        return nameTeamFirst;
+    }
+
+    public void setNameTeamFirst(String nameTeamFirst) {
+        this.nameTeamFirst = nameTeamFirst;
+    }
+
+    public String getNameTeamSecond() {
+        return nameTeamSecond;
+    }
+
+    public void setNameTeamSecond(String nameTeamSecond) {
+        this.nameTeamSecond = nameTeamSecond;
+    }
+
+    public int getGoalCountFirst() {
+        return goalCountFirst;
+    }
+
+    public void setGoalCountFirst(int goalCountFirst) {
+        this.goalCountFirst = goalCountFirst;
+    }
+
+    public int getGoalCountSecond() {
+        return goalCountSecond;
+    }
+
+    public void setGoalCountSecond(int goalCountSecond) {
+        this.goalCountSecond = goalCountSecond;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+}
