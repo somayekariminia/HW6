e2b25e0f0cb5f459f22338c012cc14bed896417b
somaye6868 <somayekariminia6868@gmail.com>
2022-09-11 17:48:54 +0430

    change getWinCount

--- src/model/Club.java
+++ src/model/Club.java
@@ -1,5 +1,57 @@
 package model;
 
 public class Club {
+    private String name;
+    private int plays;
+    private double score;
+    private int winCount;
+    private int lossCount;
 
+    public Club(String name, int plays, double score, int winCount, int lossCount) {
+        this.name = name;
+        this.plays = plays;
+        this.score = score;
+        this.winCount = winCount;
+        this.lossCount = lossCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlays() {
+        return plays;
+    }
+
+    public void setPlays(int plays) {
+        this.plays = plays;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public int getWinCount() {
+        return winCount;
+    }
+
+    public void setWinCount(int winCount) {
+        this.winCount = winCount;
+    }
+
+    public int getLossCount() {
+        return lossCount;
+    }
+
+    public void setLossCount(int lossCount) {
+        this.lossCount = lossCount;
+    }
 }
