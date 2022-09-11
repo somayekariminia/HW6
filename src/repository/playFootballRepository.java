54ad6ec99bf04dab08e80eec47800c3761416414
somaye6868 <somayekariminia6868@gmail.com>
2022-09-12 01:06:14 +0430

    add creat method

--- src/repository/playFootballRepository.java
+++ src/repository/playFootballRepository.java
@@ -1,5 +1,14 @@
 package repository;
 
-public class playRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class playFootballRepository {
+    public void creatTablePlay() throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("CREATE table play(id  SERIAL PRIMARY KEY NOT NULL,team1 varchar(50),team2 varchar(50),count_team1 int,count_team2 int,state varchar(10))");
+        preparedStatement.executeUpdate();
+    }
 
 }
