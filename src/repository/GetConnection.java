4779ec35b99e68b7098d30fdd1b78732464ada23
somaye6868 <somayekariminia6868@gmail.com>
2022-09-11 23:38:27 +0430

    add GetConnection class

---
+++ src/repository/GetConnection.java
@@ -0,0 +1,9 @@
+package repository;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class GetConnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/maktab", "postgres", "12345") ;
+    }
+}
