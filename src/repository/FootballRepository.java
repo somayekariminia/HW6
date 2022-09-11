18c3074b61390a44385e15909eaf2b255af09c35
somaye6868 <somayekariminia6868@gmail.com>
2022-09-12 00:16:43 +0430

    add creat method

--- src/repository/FootballRepository.java
+++ src/repository/FootballRepository.java
@@ -1,4 +1,21 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class FootballRepository {
+    public void creat() throws SQLException, SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE clup( id  SERIAL PRIMARY KEY NOT NULL," +
+                "nameClup VARCHAR(50) unique ," +
+                "countPlay int," +
+                "draw int," +
+                "forgoal int," +
+                "diffgoal int," +
+                "countWin int,countLoss int," +
+                "countEqual int ,score decimal)");
+        preparedStatement.executeUpdate();
+    }
+
 }
