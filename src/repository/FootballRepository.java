f818453c44c763d96e155b8d22a787dd46f86e4a
somaye6868 <somayekariminia6868@gmail.com>
2022-09-13 00:45:32 +0430

    edit select method

--- src/repository/FootballRepository.java
+++ src/repository/FootballRepository.java
@@ -1,5 +1,6 @@
 package repository;
 
+import model.Club;
 import model.FootballClub;
 
 import java.sql.Connection;
@@ -43,17 +44,17 @@ public class FootballRepository {
         preparedStatement.setString(1, name);
         preparedStatement.executeUpdate();
     }
-    public void update(FootballClub clup) throws SQLException {
+    public void update(FootballClub club) throws SQLException {
         Connection connection = GetConnection.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("update  football" + " set countplay=?,draw=?,forgoal=?,countWin=?,countLoss=?,countEqual=?,score=? " + " where nameClup=?");
-        preparedStatement.setInt(1, clup.getPlays());
-        preparedStatement.setInt(2, clup.getGoalForCount());
-        preparedStatement.setInt(3, clup.getGoalAgainst());
-        preparedStatement.setInt(4, clup.getWinCount());
-        preparedStatement.setInt(5, clup.getLossCount());
-        preparedStatement.setInt(6, clup.getEqual());
-        preparedStatement.setDouble(7, clup.getScore());
-        preparedStatement.setString(8, clup.getName());
+        preparedStatement.setInt(1, club.getPlays());
+        preparedStatement.setInt(2, club.getGoalForCount());
+        preparedStatement.setInt(3, club.getGoalAgainst());
+        preparedStatement.setInt(4, club.getWinCount());
+        preparedStatement.setInt(5, club.getLossCount());
+        preparedStatement.setInt(6, club.getEqual());
+        preparedStatement.setDouble(7, club.getScore());
+        preparedStatement.setString(8, club.getName());
         preparedStatement.executeUpdate();
     }
     public List<FootballClub> select() throws SQLException {
@@ -62,7 +63,7 @@ public class FootballRepository {
         ResultSet resultSet = preparedStatement.executeQuery();
         List<FootballClub> list = new ArrayList<>();
         while (resultSet.next()) {
-            FootballClub clup = new FootballClub(
+           FootballClub club= new FootballClub(
                     resultSet.getString(1),
                     resultSet.getInt(2),
                     resultSet.getInt(3),
@@ -72,7 +73,7 @@ public class FootballRepository {
                     resultSet.getInt(7),
                     resultSet.getInt(8),
                     resultSet.getDouble(9));
-            list.add(clup);
+            list.add((club);
         }
         return list;
     }
@@ -87,6 +88,26 @@ public class FootballRepository {
                 flage = true;
         }
         return flage;
+    }
+    public FootballClub selectByname(String name) throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from football where nameclub=? ");
+        preparedStatement.setString(1,name);
+        ResultSet resultSet = preparedStatement.executeQuery();
+       FootballClub club=null;
+        while (resultSet.next()) {
+             club = new FootballClub(
+                    resultSet.getString(1),
+                    resultSet.getInt(2),
+                    resultSet.getInt(3),
+                    resultSet.getInt(4),
+                    resultSet.getInt(5),
+                    resultSet.getInt(6),
+                    resultSet.getInt(7),
+                    resultSet.getInt(8),
+                    resultSet.getDouble(9));
+        }
+        return club;
     }
 
 }
