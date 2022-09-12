9c6d5d5ebf432908e7f0dadd146dc815d9ce9da2
somaye6868 <somayekariminia6868@gmail.com>
2022-09-13 01:06:25 +0430

    edit League

--- src/service/League.java
+++ src/service/League.java
@@ -1,12 +1,16 @@
 package service;
 
 import model.Club;
+import model.FootballClub;
+import model.Play;
 
 import java.sql.SQLException;
+import java.util.List;
+
 public interface League {
     public void addClubToLeague(Club club) throws SQLException;
     public void removeTheClubOfLeague(String name) throws SQLException;
-    public  void addGamesBetweenTwoClub() throws SQLException;
-    public void viewInformationClub( String name) throws SQLException;
-    public void seeTheLeagueTable() throws SQLException;
+    public  void addGamesBetweenTwoClub(Play play) throws SQLException;
+    public Club viewInformationClub( String name) throws SQLException;
+    public List seeTheLeagueTable() throws SQLException;
 }
