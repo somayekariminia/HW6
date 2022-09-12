c52175627cebfe2cd41b0c558db2b90dfaec4194
somaye6868 <somayekariminia6868@gmail.com>
2022-09-12 17:00:25 +0430

    implement methods class

--- src/service/League.java
+++ src/service/League.java
@@ -1,8 +1,10 @@
 package service;
 
+import model.Club;
+
 import java.sql.SQLException;
 public interface League {
-    public void addClubToLeague() throws SQLException;
+    public void addClubToLeague(Club club) throws SQLException;
     public void removeTheClubOfLeague(String name) throws SQLException;
     public  void addGamesBetweenTwoClub() throws SQLException;
     public void viewInformationClub( String name) throws SQLException;
--- src/service/LeagueFootball.java
+++ src/service/LeagueFootball.java
@@ -1,4 +1,32 @@
 package service;
 
-public class LeagueFootball {
+import model.Club;
+
+import java.sql.SQLException;
+
+public class LeagueFootball implements League {
+    @Override
+    public void addClubToLeague(Club club) throws SQLException {
+
+    }
+
+    @Override
+    public void removeTheClubOfLeague(String name) throws SQLException {
+
+    }
+
+    @Override
+    public void addGamesBetweenTwoClub() throws SQLException {
+
+    }
+
+    @Override
+    public void viewInformationClub(String name) throws SQLException {
+
+    }
+
+    @Override
+    public void seeTheLeagueTable() throws SQLException {
+
+    }
 }
