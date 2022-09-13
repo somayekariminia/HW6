2e58b834d51ef4bcbaddfb5111ef79bd050948b8
somaye6868 <somayekariminia6868@gmail.com>
2022-09-13 16:28:59 +0430

    add LeagueVolleyballImp class

---
+++ src/service/LeagueVolleyballImp.java
@@ -0,0 +1,34 @@
+package service;
+
+import model.Club;
+import model.Play;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LeagueVolleyballImp implements League {
+    @Override
+    public void addClubToLeague(Club club) throws SQLException {
+
+    }
+
+    @Override
+    public void removeTheClubOfLeague(String name) throws SQLException {
+
+    }
+
+    @Override
+    public void addGamesBetweenTwoClub(Play play) throws SQLException {
+
+    }
+
+    @Override
+    public Club viewInformationClub(String name) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List seeTheLeagueTable() throws SQLException {
+        return null;
+    }
+}
