bc39dd8b4c0da182cddfe7ccc4a5a52f5c34679c
somaye6868 <somayekariminia6868@gmail.com>
2022-09-14 08:30:05 +0430

    add insertPlayVolleyball and deletPlayVolleyball and getListOfPlay in PlayVolleyballRepositoryclass

---
+++ src/repository/PlayVolleyballRepository.java
@@ -0,0 +1,46 @@
+package repository;
+
+import model.Play;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayVolleyballRepository {
+
+    public void insertPlayVolleyball(Play play) throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        String str = "insert into playvolleyball(team1,team2,count_team1,count_team2,result) values(?,?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(str);
+        preparedStatement.setString(1, play.getNameTeamFirst());
+        preparedStatement.setString(2, play.getNameTeamSecond());
+        preparedStatement.setInt(3, play.getGoalCountFirst());
+        preparedStatement.setInt(4, play.getGoalCountSecond());
+        preparedStatement.setString(5, play.getResult());
+        preparedStatement.executeUpdate();
+    }
+    public void deletePlayVolleyball(String name) throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("delete from playvolleyball where team1=?");
+        preparedStatement.setString(1, name);
+    }
+    public List<Play> getListOfPlays(String name) throws SQLException {
+        Connection connection = GetConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from playvolleyball where team1=?");
+        preparedStatement.setString(1,name);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<Play> list = new ArrayList<>();
+        while (resultSet.next()) {
+            Play play = new Play(resultSet.getString("team1"),
+                    resultSet.getString("team2"),
+                    resultSet.getInt("count_team1"),
+                    resultSet.getInt("count_team2"),
+                    resultSet.getString("result"));
+            list.add(play);
+        }
+        return list;
+    }
+}
