b8cbc5f0d3486a7a114b76cca1a5c58d3972add3
somaye6868 <somayekariminia6868@gmail.com>
2022-09-14 14:04:49 +0430

    add getPlaysOfTable method to class GameFootball

--- src/service/GameFootballImpl.java
+++ src/service/GameFootballImpl.java
@@ -1,20 +1,28 @@
 package service;
 
 import model.Play;
+import repository.PlaysVolleyballRepository;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameFootballImpl implements Game{
+    PlaysVolleyballRepository playsVolleyballRepository=new PlaysVolleyballRepository();
+    List<Play> plays=new ArrayList<>();
     @Override
-    public void addPlayToTable(Play play) {
-
+    public void addPlayToTable(Play play) throws SQLException {
+      playsVolleyballRepository.insertPlayVolleyball(play);
     }
 
     @Override
-    public void deletePlayOfTable(String name) {
-
+    public void deletePlayOfTable(String name) throws SQLException {
+playsVolleyballRepository.deletePlayVolleyball(name);
     }
 
     @Override
-    public void getPlaysOfTable(String name) {
+    public List<Play> getPlaysOfTable(String name) throws SQLException {
+         return plays=playsVolleyballRepository.getListOfPlays(name);
 
     }
 }
